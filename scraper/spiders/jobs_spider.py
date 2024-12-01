a1888761ef96fbde4763be9857734648073a840d
twonumberfortyfives <sma4no.7@gmail.com>
2024-12-01 02:19:51 +0200

    got robot.txt..... :C

--- scraper/spiders/jobs_spider.py
+++ scraper/spiders/jobs_spider.py
@@ -1,1 +1,16 @@
+from pathlib import Path
 
+import scrapy
+from scrapy.http import Response
+
+
+class JobsSpider(scrapy.Spider):
+    name = "jobs"
+    allowed_domains = ["jobs.dou.ua"]
+    start_urls = [
+        "https://jobs.dou.ua/vacancies/?category=Python"
+    ]
+
+    def parse(self, response: Response, **kwargs):
+        filename = "jobs.html"
+        Path(filename).write_bytes(response.body)
